c944d61790da494902d5c0b8168a3eb7edf4a8a6
Artur-Shvets <arturshvets477@gmail.com>
2022-05-31 14:21:44 +0300

    0.0.2

--- highlight parse.js
+++ highlight parse.js
@@ -1,106 +1,230 @@
-"use strict"
+'use strict';
 
 const patterns = {
-  regex: /\/.+\/\w*/gm,
-  comments: /\/\/.+/gm,
-  backQuotes: /`(.*?)\${(.*?)}(.*?)`/gm,
-  strings: /(["'])(.+?)\1/gm,
-  arguments: /\(([^(]+?)\).*?=>/gm,
-  keyWords: /\b(=>|break|case|class|catch|const|continue|debugger|default|delete|do|else|export|extends|finally|function|if|import|in|instanceof|let|new|return|super|switch|this|throw|try|typeof|var|void|while|with|yield)\b/gm,
-  literals: /(null|true|false)\b/gm,
-  interfaces: /(window|document|console)\./gm,
-  objects: /(?<=\.)(\w+)/gm,
-  numbers: /\b\d+?\b/gm,
-  words: /\b\w+?\b/gm,
-  symbols: /[^\w\s~]/gm,
-  spaces: /\s/gm,
-  funcNameAndArgs: /(?<=function)(\s*?)(\w+)(\s*?\(\s*?)(.*?)(\s*?\))/gm,
-  allFuncNames: ''
-}
+  regex: /\/.+\/\w*/g,
+  comments: /\/\/.+/g,
+  // oneLineMultiComments: /\/\*(.+)*\//g,
+  openMultiComments: /\/\*.*/g,
+  closeMultiComments: /.*\*\//g,
+  oneLineMultiComments: /\/\*.*\*\//g,
+  openBackQuotes: /`.+/g,
+  closeBackQuotes: /.+`/g,
+  oneLineBackQuotes: /`.+`/g,
+  templateStrings: /(\${)(.+?)(})/g,
+  strings: /'.*?'|".*?"/g,
+  arguments: /\(([^(]+?)\).*?=>/g,
+  keyWords:
+    /\b(=>|break|case|class|catch|const|continue|debugger|default|delete|do|else|export|extends|finally|function|for|if|import|in|instanceof|let|new|return|super|switch|this|throw|try|typeof|var|void|while|with|yield)\b/g,
+  keys: /\w+(?=:)/g,
+  literals: /(null|true|false|undefined)\b/g,
+  interfaces: /(window|document|console)\b/g,
+  objects: /(?<=\.)(\w+)/g,
+  numbers: /\b\d+?\b/g,
+  words: /\b\w+?\b/g,
+  symbols: /[^\w\s~]/g,
+  spaces: /\s/g,
+  funcNameAndArgs: /(?<=function)(\s+?)(\S+)(\s*?\(\s*?)(.*?)(\s*?\))/g,
+  callFunctions: /\w+(?=\s*\()/g,
+  allFuncNames: '',
+};
 
 let composeText;
 
 let functionNames = [];
 
-function addNameOfFunc (name) {
+function addNameOfFunc(name) {
   functionNames.push(name);
   if (patterns.allFuncNames) {
-    patterns.allFuncNames = new RegExp('('+functionNames.join('|')+')', 'gm');
+    patterns.allFuncNames = new RegExp(
+      '(' + functionNames.join('|') + ')',
+      'gm'
+    );
   } else {
-    patterns.allFuncNames = new RegExp('('+name+')', 'gm');
+    patterns.allFuncNames = new RegExp('(' + name + ')', 'gm');
   }
 }
 
-function getHighlightText (rowText) {
+let isOpenComment = false;
+let openBackQuotes = false;
+
+function getHighlightText(rowText) {
   composeText = [];
-  rowText = rowText.replace(patterns.regex, (g0, index) => {
-    composeText[index] = `<span class="cyan-string">${g0}</span>`;
-    return '~'.repeat(g0.length)
+  console.log('rowText: ', rowText);
+
+  if (!rowText) {
+    return `<br>`;
+  }
+
+  if (isOpenComment && !patterns.closeMultiComments.test(rowText)) {
+    return `<span class="gray-string">${rowText}</span>`;
+  }
+
+  rowText = rowText.replace(patterns.oneLineMultiComments, (g0, index) => {
+    g0 = g0.replace(/</gm, '&lt;');
+    g0 = g0.replace(/>/gm, '&gt;');
+    composeText[index] = `<span class="gray-string">${g0}</span>`;
+    return '~'.repeat(g0.length);
   });
+
+  rowText = rowText.replace(patterns.openMultiComments, (g0, index) => {
+    g0 = g0.replace(/</gm, '&lt;');
+    g0 = g0.replace(/>/gm, '&gt;');
+    isOpenComment = true;
+    composeText[index] = `<span class="gray-string">${g0}</span>`;
+    return '~'.repeat(g0.length);
+  });
+
+  rowText = rowText.replace(patterns.closeMultiComments, (g0, index) => {
+    isOpenComment = false;
+    g0 = g0.replace(/</gm, '&lt;');
+    g0 = g0.replace(/>/gm, '&gt;');
+    composeText[index] = `<span class="gray-string">${g0}</span>`;
+    return '~'.repeat(g0.length);
+  });
+
   rowText = rowText.replace(patterns.comments, (g0, index) => {
-    composeText[index] = `<span class="block gray-string gray-shadow">${g0}</span>`;
-    return '~'.repeat(g0.length)
+    g0 = g0.replace(/</gm, '&lt;');
+    g0 = g0.replace(/>/gm, '&gt;');
+    composeText[index] = `<span class="gray-string">${g0}</span>`;
+    return '~'.repeat(g0.length);
   });
-  rowText = rowText.replace(patterns.backQuotes, (g0, g1, g2, g3, index) => {
-    composeText[index] = `<span class="green-string">`;
-    return '~'.repeat(g0.length)
+
+  //==========================================================================================
+  rowText = rowText.replace(patterns.oneLineBackQuotes, (g0, index) => {
+    let subText = [];
+    g0 = g0.replace(/</gm, '&lt;');
+    g0 = g0.replace(/>/gm, '&gt;');
+    g0 = g0.replace(
+      patterns.templateStrings,
+      (subG0, subG1, subG2, subG3, subIndex) => {
+        subText[
+          subIndex
+        ] = `<span class="cyan-string">${subG1}</span><span class="block green-string green-shadow">${subG2}</span><span class="cyan-string">${subG3}</span>`;
+        return '~'.repeat(subG0.length);
+      }
+    );
+    g0.replace(/[^~]+/gm, (subG0, subIndex) => {
+      subText[subIndex] = subG0;
+      return '~'.repeat(subG0.length);
+    });
+    subText = subText.join('');
+    composeText[index + 1] =
+      '<span class="block green-string green-shadow">' + subText + '</span>';
+    return '~'.repeat(g0.length);
   });
-  rowText = rowText.replace(patterns.strings, (g0, g1, g2, index) => {
-    composeText[index+1] = `<span class="block green-string green-shadow">${g2}</span>`;
-    return g1+('~'.repeat(g2.length))+g1
+
+  //==========================================================================================
+  rowText = rowText.replace(patterns.regex, (g0, index) => {
+    g0 = g0.replace(/</gm, '&lt;');
+    g0 = g0.replace(/>/gm, '&gt;');
+    composeText[
+      index
+    ] = `<span class="block cyan-string cyan-shadow">${g0}</span>`;
+    return '~'.repeat(g0.length);
   });
+
+  rowText = rowText.replace(patterns.strings, (g0, index) => {
+    g0 = g0.replace(/</gm, '&lt;');
+    g0 = g0.replace(/>/gm, '&gt;');
+    composeText[
+      index
+    ] = `<span class="block green-string green-shadow">${g0}</span>`;
+    return '~'.repeat(g0.length);
+  });
+
+  // ______________________________________________________________Before
+
+  rowText = rowText.replace(patterns.keys, (g0, index) => {
+    composeText[index] = `<span class="orange-string">${g0}</span>`;
+    return '~'.repeat(g0.length);
+  });
+
   rowText = rowText.replace(patterns.arguments, (g0, g1, index) => {
-    g1 = g1.replace(/\w+/gm, (subg0, subindex) => {
-      composeText[index+1+subindex] = `<span class="block strong-orange-string strong-orange-shadow">${subg0}</span>`;
-      return '~'.repeat(subg0.length)
-    })
-    return `(${g1}) =>`
+    g1 = g1.replace(/\w+/gm, (subG0, subIndex) => {
+      composeText[
+        index + 1 + subIndex
+      ] = `<span class="block strong-orange-string strong-orange-shadow">${subG0}</span>`;
+      return '~'.repeat(subG0.length);
+    });
+    return `(${g1}) =>`;
   });
-  rowText = rowText.replace(patterns.funcNameAndArgs, (g0, g1, g2, g3, g4, g5, index) => {
-    addNameOfFunc(g2);
-    composeText[index+g1.length] = `<span class="block blue-string blue-shadow">${g2}</span>`;
-    g4 = g4.replace(/\w+/gm, (subg0, subindex) => {
-      composeText[index+g1.length+g2.length+g3.length+subindex] = `<span class="block strong-orange-string strong-orange-shadow">${subg0}</span>`;
-      return '~'.repeat(subg0.length)
-    })
-    return g1+'~'.repeat(g2.length)+g3+g4+g5
-  });
+
+  rowText = rowText.replace(
+    patterns.funcNameAndArgs,
+    (g0, g1, g2, g3, g4, g5, index) => {
+      addNameOfFunc(g2);
+      composeText[
+        index + g1.length
+      ] = `<span class="block blue-string blue-shadow">${g2}</span>`;
+      g4 = g4.replace(/\w+/gm, (subg0, subindex) => {
+        composeText[
+          index + g1.length + g2.length + g3.length + subindex
+        ] = `<span class="block strong-orange-string strong-orange-shadow">${subg0}</span>`;
+        return '~'.repeat(subg0.length);
+      });
+      return g1 + '~'.repeat(g2.length) + g3 + g4 + g5;
+    }
+  );
+
   rowText = rowText.replace(patterns.allFuncNames, (g0, g1, index) => {
-    composeText[index] = `<span class="block blue-string blue-shadow">${g1}</span>`;
-    return '~'.repeat(g1.length)
+    composeText[
+      index
+    ] = `<span class="block blue-string blue-shadow">${g1}</span>`;
+    return '~'.repeat(g1.length);
   });
+
   rowText = rowText.replace(patterns.keyWords, (g0, g1, index) => {
     composeText[index] = `<span class="purple-string">${g1}</span>`;
-    return '~'.repeat(g1.length)
+    return '~'.repeat(g1.length);
   });
+
   rowText = rowText.replace(patterns.literals, (g0, g1, index) => {
-    composeText[index] = `<span class="block red-string red-shadow">${g1}</span>`;
-    return '~'.repeat(g1.length)
+    composeText[
+      index
+    ] = `<span class="block red-string red-shadow">${g1}</span>`;
+    return '~'.repeat(g1.length);
   });
+
   rowText = rowText.replace(patterns.interfaces, (g0, g1, index) => {
     composeText[index] = `<span class="orange-string">${g1}</span>`;
-    return '~'.repeat(g1.length)+'.'
+    return '~'.repeat(g1.length);
   });
+
+  rowText = rowText.replace(patterns.callFunctions, (g0, index) => {
+    composeText[
+      index
+    ] = `<span class="block blue-string blue-shadow">${g0}</span>`;
+    return '~'.repeat(g0.length);
+  });
+
   rowText = rowText.replace(patterns.objects, (g0, g1, index) => {
-    if (g1 == 'length') {
-      composeText[index] = `<span class="block white-string white-shadow">${g1}</span>`;
-    } else {
-      composeText[index] = `<span class="block blue-string blue-shadow">${g1}</span>`;
-    }
-    return '~'.repeat(g1.length)
+    composeText[
+      index
+    ] = `<span class="block orange-string orange-shadow">${g1}</span>`;
+    return '~'.repeat(g1.length);
   });
+
   rowText = rowText.replace(patterns.numbers, (g0, index) => {
-    composeText[index] = `<span class="block strong-orange-string strong-orange-shadow">${g0}</span>`;
-    return '~'.repeat(g0.length)
+    composeText[
+      index
+    ] = `<span class="block strong-orange-string strong-orange-shadow">${g0}</span>`;
+    return '~'.repeat(g0.length);
   });
+
+  // ______________________________________________________________After
   rowText.replace(patterns.words, (g0, index) => {
-    composeText[index] = `<span class="block white-string white-shadow">${g0}</span>`;
+    composeText[
+      index
+    ] = `<span class="block white-string white-shadow">${g0}</span>`;
   });
+
   rowText.replace(patterns.symbols, (g0, index) => {
     composeText[index] = `<span class="cyan-string">${g0}</span>`;
   });
+
   rowText.replace(patterns.spaces, (g0, index) => {
     composeText[index] = `<span>${g0}</span>`;
   });
-  return composeText.join('')+'\n';
+
+  return composeText.join('') + '\n';
 }
--- structure parse.js
+++ structure parse.js
@@ -1,4 +1,4 @@
-"use strict"
+'use strict';
 
 let input = document.getElementById('input');
 let inputText;
@@ -10,16 +10,15 @@ let rowBlock = false;
 let mainBlock = false;
 let identBlock;
 
-
-function addColorForBlock (block, rowText) {
+function addColorForBlock(block, rowText) {
   if (block.classList.contains('row-block')) {
-    if (/(let|function|if)/gm.test(rowText)) {
+    if (/\b(let|function|if)\b/gm.test(rowText)) {
       block.classList.add('purple-shadow');
     }
   } else {
-    if (/(let|function|if)/gm.test(rowText)) {
+    if (/\b(let|function|if)\b/gm.test(rowText)) {
       block.classList.add('purple-shadow');
-    } else if (/(for|forEach)\s/gm.test(rowText)) {
+    } else if (/\b(for|forEach)\b/gm.test(rowText)) {
       block.classList.add('blue-shadow');
     } else if (/(window|document|console)\./gm.test(rowText)) {
       block.classList.add('orange-shadow');
@@ -27,18 +26,18 @@ function addColorForBlock (block, rowText) {
   }
 }
 
-function createParent (rowText) {
+function createParent(rowText) {
   mainParent = document.createElement('div');
   mainParent.classList.add('main-block', 'main-parent');
   mainBlock = mainParent;
   mainParent.setAttribute('contenteditable', 'true');
-  addColorForBlock (mainParent, rowText);
+  addColorForBlock(mainParent, rowText);
   input.append(mainParent);
   createRow(rowText);
   mainParent.append(rowBlock);
 }
 
-function createMainBlock (rowText) {
+function createMainBlock(rowText) {
   mainBlock = document.createElement('div');
   mainBlock.classList.add('main-block');
   subBlock.append(mainBlock);
@@ -49,40 +48,30 @@ function createMainBlock (rowText) {
   mainBlock.append(subBlock);
 }
 
-function createSubBlock () {
+function createSubBlock() {
   subBlock = document.createElement('div');
   subBlock.classList.add('sub-block');
   mainBlock.append(subBlock);
 }
 
-
-function createRowBlock (rowText) {
+function createRowBlock(rowText) {
   rowBlock = document.createElement('div');
   rowBlock.classList.add('row-block');
-  rowText = rowText.replace(/^\s+/, (g0, index) => {
-    return ''
-  });
   addColorForBlock(rowBlock, rowText);
   rowBlock.innerHTML = getHighlightText(rowText);
 }
 
-function createRow (rowText) {
+function createRow(rowText) {
   rowBlock = document.createElement('div');
   rowBlock.classList.add('row');
-  rowText = rowText.replace(/^\s*/, (g0, index) => {
-    return ''
-  });
   rowBlock.innerHTML = getHighlightText(rowText);
 }
 
-function createIndent () {
-  identBlock = document.createElement('div');
-  identBlock.classList.add('ident-block');
-  identBlock.innerText = '\n';
-  input.append(identBlock);
+function createEmpty() {
+  rowBlock = document.createElement('br');
 }
 
-function getPreviousBlocks () {
+function getPreviousBlocks() {
   if (mainBlock.classList.contains('main-parent') != true) {
     subBlock = mainBlock.parentElement;
     mainBlock = subBlock.parentElement;
@@ -93,50 +82,71 @@ function getPreviousBlocks () {
   }
 }
 
-let countIdent = 0;
+let emptyRows = 0;
+function checkIsEmpty(rowText) {
+  if (!rowText) {
+    emptyRows++;
+    if (emptyRows == 2) {
+      emptyRows = 0;
+      return false;
+    } else {
+      return true;
+    }
+  }
+  if (rowText && emptyRows == 1) {
+    emptyRows = 0;
+    return false;
+  }
+}
+
 function parserCore() {
   inputText = input.innerText.split('\n');
   input.innerText = null;
-  inputText.forEach((rowText) => {
-    openBrace = /{$/gm.test(rowText);
-    closedBrace = /^\s*?}/gm.test(rowText);
-    if (openBrace && !closedBrace) {
-      if (mainParent) {
-        createMainBlock(rowText);
-      } else {
-        createParent(rowText);
-        createSubBlock();
-      }
-    } else if (!openBrace && closedBrace) {
-      createRow(rowText);
-      mainBlock.append(rowBlock);
-      getPreviousBlocks();
-    } else if (openBrace && closedBrace) {
-      createRow(rowText);
-      mainBlock.append(rowBlock);
-      createSubBlock();
-    } else {
-      if (mainParent) {
-        createRowBlock(rowText);
-        if (subBlock) {
-          subBlock.append(rowBlock);
-        } else {
-          mainBlock.append(rowBlock);
-        }
-      } else {
-        if (rowText.length == 0) {
-          countIdent += 1;
-          if (countIdent % 2 == 0) {
-            countIdent = 0;
-            createIndent();
-          }
+  inputText.forEach(rowText => {
+    rowText = rowText.replace(/^\s*/, '');
+    let isEmpty = checkIsEmpty(rowText);
+    if (isEmpty || rowText) {
+      openBrace = /[{[(]\s*(\/\/|$)/g.test(rowText);
+      closedBrace = /^\s*?[}\])]/g.test(rowText);
+      if (openBrace && !closedBrace) {
+        if (mainParent) {
+          createMainBlock(rowText);
         } else {
           createParent(rowText);
-          mainParent = false;
-          mainBlock = false;
-          subBlock = false;
+          createSubBlock();
+        }
+      } else if (!openBrace && closedBrace) {
+        createRow(rowText);
+        mainBlock.append(rowBlock);
+        getPreviousBlocks();
+      } else if (openBrace && closedBrace) {
+        createRow(rowText);
+        mainBlock.append(rowBlock);
+        createSubBlock();
+      } else {
+        if (mainParent) {
+          if (isEmpty) {
+            createEmpty();
+          } else {
+            createRowBlock(rowText);
+          }
+          if (subBlock) {
+            subBlock.append(rowBlock);
+          } else {
+            mainBlock.append(rowBlock);
+          }
+        } else {
+          if (isEmpty) {
+            createEmpty();
+            input.append(rowBlock);
+          } else {
+            createParent(rowText);
+            mainParent = false;
+            mainBlock = false;
+            subBlock = false;
+          }
         }
       }
     }
-  })
+  });
 }
